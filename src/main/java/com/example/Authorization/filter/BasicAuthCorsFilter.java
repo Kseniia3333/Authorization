bdf93e02cc854ab6dbf27b997acbb8ba5f790e12
Kseniia313 <kshome@mail.ru>
2024-09-09 12:07:01 +0300

    Initial commit

--- src/main/java/com/example/Authorization/filter/BasicAuthCorsFilter.java
+++ src/main/java/com/example/Authorization/filter/BasicAuthCorsFilter.java
@@ -7,14 +7,14 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
-public class BasicAuthCorsFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest,
-                                    HttpServletResponse httpServletResponse,
-                                    FilterChain filterChain)
-            throws ServletException, IOException {
-        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
-    }
-}
+//@Component
+//public class BasicAuthCorsFilter extends OncePerRequestFilter {
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+//                                    HttpServletResponse httpServletResponse,
+//                                    FilterChain filterChain)
+//            throws ServletException, IOException {
+//        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
+//        filterChain.doFilter(httpServletRequest, httpServletResponse);
+//    }
+//}
